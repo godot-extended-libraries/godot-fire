a398dd47d404e1b10c94075547960cd901ee2c60
Rémi Verschelde <rverschelde@gmail.com>
2020-02-11 16:32:26 +0100

    Integrated NavigationServer and Navigation2DServer.
    - Added Navigation Agents and Obstacles.
    - Integrated Collision Avoidance.
    
    This work has been kindly sponsored by IMVU.
    
    Updated documentation for new nav server functionality.
    
    Added additional information for some of the methods.
    
    Fixed misuse of the word Agent in the NavigationObstacle classes
    
    Added additional information to explain behavior if get_next_location does not have a nav path
    
    Added additional information to some methods to mention that the velocity computed signal is physics safe. Also added some new information for the navigation mesh instance class.
    
    Remove unused WinAPI includes/defines from RVO2.

--- modules/gdnavigation/navigation_mesh_generator.h
+++ modules/gdnavigation/navigation_mesh_generator.h
@@ -1,5 +1,5 @@
 /*************************************************************************/
-/*  register_types.h                                                     */
+/*  navigation_mesh_generator.h                                          */
 /*************************************************************************/
 /*                       This file is part of:                           */
 /*                           GODOT ENGINE                                */
@@ -28,10 +28,56 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 
-#ifndef RECAST_REGISTER_TYPES_H
-#define RECAST_REGISTER_TYPES_H
+#ifndef NAVIGATION_MESH_GENERATOR_H
+#define NAVIGATION_MESH_GENERATOR_H
 
-void register_recast_types();
-void unregister_recast_types();
+#ifndef _3D_DISABLED
 
-#endif // RECAST_REGISTER_TYPES_H
+#include "scene/3d/navigation_mesh_instance.h"
+
+#include <Recast.h>
+
+#ifdef TOOLS_ENABLED
+struct EditorProgress;
+#endif
+
+class NavigationMeshGenerator : public Object {
+	GDCLASS(NavigationMeshGenerator, Object);
+
+	static NavigationMeshGenerator *singleton;
+
+protected:
+	static void _bind_methods();
+
+	static void _add_vertex(const Vector3 &p_vec3, Vector<float> &p_verticies);
+	static void _add_mesh(const Ref<Mesh> &p_mesh, const Transform &p_xform, Vector<float> &p_verticies, Vector<int> &p_indices);
+	static void _add_faces(const PoolVector3Array &p_faces, const Transform &p_xform, Vector<float> &p_verticies, Vector<int> &p_indices);
+	static void _parse_geometry(Transform p_accumulated_transform, Node *p_node, Vector<float> &p_verticies, Vector<int> &p_indices, int p_generate_from, uint32_t p_collision_mask, bool p_recurse_children);
+
+	static void _convert_detail_mesh_to_native_navigation_mesh(const rcPolyMeshDetail *p_detail_mesh, Ref<NavigationMesh> p_nav_mesh);
+	static void _build_recast_navigation_mesh(
+			Ref<NavigationMesh> p_nav_mesh,
+#ifdef TOOLS_ENABLED
+			EditorProgress *ep,
+#endif
+			rcHeightfield *hf,
+			rcCompactHeightfield *chf,
+			rcContourSet *cset,
+			rcPolyMesh *poly_mesh,
+			rcPolyMeshDetail *detail_mesh,
+			Vector<float> &vertices,
+			Vector<int> &indices);
+
+public:
+	static NavigationMeshGenerator *get_singleton();
+
+	NavigationMeshGenerator();
+	~NavigationMeshGenerator();
+
+	void bake(Ref<NavigationMesh> p_nav_mesh, Node *p_node);
+	void clear(Ref<NavigationMesh> p_nav_mesh);
+};
+
+#endif
+
+#endif // NAVIGATION_MESH_GENERATOR_H
