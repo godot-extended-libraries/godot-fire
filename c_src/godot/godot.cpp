44f97d4aee189c4ca7d853b5b00201601522e7e5
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-09-18 12:34:31 -0700

    Have proper errors.

--- c_src/godot/godot.cpp
+++ c_src/godot/godot.cpp
@@ -5,50 +5,40 @@ static OS_Server os;
 UNIFEX_TERM init(UnifexEnv *env, MyState *state, char **in_strings, unsigned int list_length) {
 	int err = OK;
 	if (state) {
-		UNIFEX_TERM res = init_result_ok(env, state, FAILED);
-		return res;
+		return init_result_fail(env, state, "Godot is already initialized.");
 	}
 	state = unifex_alloc_state(env);
 	err = Main::setup(in_strings[0], list_length - 1, &in_strings[1]);
 	if (err != OK) {
-		return init_result_ok(env, state, 255);
-	}
-	if (!state) {
-		return init_result_ok(env, state, 255);
+		return init_result_fail(env, state, "Godot can't be setup.");
 	}
 	if (!Main::start()) {
-		return init_result_ok(env, state, 255);
-	}
-	UNIFEX_TERM res = init_result_ok(env, state, err);
-	
-	return res;
+		return init_result_fail(env, state, "Godot can't start.");
+	}	
+	return init_result_ok(env, state, err);
 }
 
 UNIFEX_TERM iteration(UnifexEnv *env, MyState *state, int delta) {
 	// delta should be a float
 	if (!state) {
-		return iteration_result_ok(env, state, 255);
+		return iteration_result_fail(env, state, "Godot is not initalized.");
 	}
 	bool err = os.get_main_loop()->iteration(delta);
 	if (err != OK) {
-		return iteration_result_ok(env, state, 255);
+		return iteration_result_fail(env, state, "Godot can't iterate.");
 	}
-	UNIFEX_TERM res = iteration_result_ok(env, state, err);
-	return res;
+	return iteration_result_ok(env, state, err);
 }
 
 UNIFEX_TERM call(UnifexEnv *env, MyState *state, char *method) {
 	if (!state) {
-		return call_result_ok(env, state, Variant::NIL, "");
+        return call_result_fail(env, state, "Godot is not initalized.");
 	}
 	if (!os.get_main_loop()->get_script_instance()) {
-		return call_result_ok(env, state, Variant::NIL, "");
+        return call_result_fail(env, state, "Godot does not have a script instance.");
 	}
 	Variant::CallError call_error;
 	Variant res = os.get_main_loop()->call(method, nullptr, 0, call_error);
-	if (call_error.error != Variant::CallError::CALL_OK) {
-		return call_result_ok(env, state, Variant::NIL, "");
-	}
 	const CharString res_char_string = String(res).utf8();
 	const char *res_string = res_char_string.get_data();
 	return call_result_ok(env, state, res.get_type(), res_string);
--- c_src/godot/godot.spec.exs
+++ c_src/godot/godot.spec.exs
@@ -5,5 +5,8 @@ interface [CNode]
 state_type "MyState"
 
 spec init(state, arguments :: [string]) :: {:ok :: label, state, code :: int}
+                                  | {:fail :: label, state, result :: string}
 spec iteration(state, delta :: int) :: {:ok :: label, state, code :: int}
-spec call(state, method :: string)  :: {:ok :: label, state, type :: int, result :: string}
+                                  | {:fail :: label, state, result :: string}
+spec call(state, method :: string)  :: {:ok :: label, state, type :: int, result_string :: string}
+                                  | {:fail :: label, state, result :: string}
