42ef5867584a9d965038d1a35f7e12a44e9727ff
TwistedTwigleg <beard.noah@gmail.com>
2020-09-22 14:17:43 -0400

    Changes: * SkeletonModification3DFABRIK: Added the forward pass back. Turns out, removing it lead to the wiggle instability issue! Now that it is back, FABRIK doesn't wiggle around when the target is getting out of range * SkeletonModification3DFABRIK: Moved chain_apply outside of the while loop when solving FABRIK, since rotation needs to only be applied once. This should help improve performance without any difference, helping offset the slight performance loss of adding the forward pass back. * SkeletonModification3DCCDIK: Fixed angle constraints and now it is working as expected.   * Turns out an assumption I was making (or something changed under the hood) was causing the issue. I assumed the get_axis_angle function would return an angle in the range -180 to 180, but this is not the case. The angle returned is always in the range 0 to 180, with the negative value in the axis as the indicator that the angle is over 180 degrees. With this worked around, the angle constraints work as expected. * SkeletonModification3D: Changed how get_modification_stack is defined in SkeletonModification3D. Now it returns a Ref<SkeletonModificationStack3D> and that allows the CI tests to run successfully. * SkeletonModification3D: _print_execution_error now does not print an error to the console if the modification is not setup. * All 3D SkeletonModification resources: Now uses _print_execution_error when checking if setup, so there are not a ton of "errors" when loading just because the resource has not been setup by the Skeleton3D node at start.

--- scene/resources/skeleton_modification_3d.h
+++ scene/resources/skeleton_modification_3d.h
@@ -127,7 +127,7 @@ public:
 	void set_execution_mode(int p_mode);
 	int get_execution_mode() const;
 
-	SkeletonModificationStack3D *get_modification_stack();
+	Ref<SkeletonModificationStack3D> get_modification_stack();
 
 	void set_is_setup(bool p_setup);
 	bool get_is_setup() const;
@@ -312,6 +312,7 @@ private:
 	Transform origin_global_pose = Transform();
 
 	void chain_backwards();
+	void chain_forwards();
 	void chain_apply();
 
 protected:
