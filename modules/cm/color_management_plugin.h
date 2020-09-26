d93e7040fa51bed21dc45e2d6286a0b7bb311ea7
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-09-25 23:30:03 -0700

    Added Rasterizer API for display color management.
    Use VisualServer::set_screen_lut to set a 3D texture for color management.
    
    Its X, Y, Z axes correspond to R, G, B channels.
    
    LUT transformation happens in Rasterizer, ensuring that all content blitted
    to screen are processed exactly once.
    
    This implements part of #26826.
    Add color management module for ICC support.
    
    This module adds Color Management to engine settings. It depends on LittleCMS
    for ICC profile handling, and LUT support in Rasterizer for rendering.
    
    This completes #26826.

--- modules/cm/color_management_plugin.h
+++ modules/cm/color_management_plugin.h
@@ -1,5 +1,5 @@
 /*************************************************************************/
-/*  rasterizer_gles2.h                                                   */
+/*  color_management_plugin.h                                            */
 /*************************************************************************/
 /*                       This file is part of:                           */
 /*                           GODOT ENGINE                                */
@@ -28,61 +28,24 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 
-#ifndef RASTERIZERGLES2_H
-#define RASTERIZERGLES2_H
+#ifndef COLOR_MANAGEMENT_PLUGIN_H
+#define COLOR_MANAGEMENT_PLUGIN_H
 
-#include "rasterizer_canvas_gles2.h"
-#include "rasterizer_scene_gles2.h"
-#include "rasterizer_storage_gles2.h"
-#include "servers/visual/rasterizer.h"
+#include "editor/editor_plugin.h"
 
-#include "shaders/lut_transform.glsl.gen.h"
+class ColorManagementPlugin : public EditorPlugin {
+	GDCLASS(ColorManagementPlugin, EditorPlugin)
 
-class RasterizerGLES2 : public Rasterizer {
+	void _on_settings_change();
+	void _register_settings();
+	void _clear_lut();
 
-	static Rasterizer *_create_current();
-
-	RasterizerStorageGLES2 *storage;
-	RasterizerCanvasGLES2 *canvas;
-	RasterizerSceneGLES2 *scene;
-
-	double time_total;
-	float time_scale;
-
-	struct State {
-		RID screen_lut;
-		Vector2 lut_texel_count;
-		Vector2 lut_chunk_count;
-		LutTransformShaderGLES2 lut_shader;
-	} state;
+protected:
+	static void _bind_methods();
 
 public:
-	virtual RasterizerStorage *get_storage();
-	virtual RasterizerCanvas *get_canvas();
-	virtual RasterizerScene *get_scene();
-
-	virtual void set_boot_image(const Ref<Image> &p_image, const Color &p_color, bool p_scale, bool p_use_filter = true);
-	virtual void set_shader_time_scale(float p_scale);
-	virtual void set_screen_lut(const Ref<Image> &p_lut, int p_h_slices, int p_v_slices);
-
-	virtual void initialize();
-	virtual void begin_frame(double frame_step);
-	virtual void set_current_render_target(RID p_render_target);
-	virtual void restore_render_target(bool p_3d_was_drawn);
-	virtual void clear_render_target(const Color &p_color);
-	virtual void blit_render_target_to_screen(RID p_render_target, const Rect2 &p_screen_rect, int p_screen = 0);
-	virtual void output_lens_distorted_to_screen(RID p_render_target, const Rect2 &p_screen_rect, float p_k1, float p_k2, const Vector2 &p_eye_center, float p_oversample);
-	virtual void end_frame(bool p_swap_buffers);
-	virtual void finalize();
-
-	static Error is_viable();
-	static void make_current();
-	static void register_config();
-
-	virtual bool is_low_end() const { return true; }
-
-	RasterizerGLES2();
-	~RasterizerGLES2();
+	ColorManagementPlugin();
+	virtual ~ColorManagementPlugin() {}
 };
 
-#endif // RASTERIZERGLES2_H
+#endif // COLOR_MANAGEMENT_PLUGIN_H
