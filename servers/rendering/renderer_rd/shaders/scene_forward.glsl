e3678031bfd0a8b14633a4568460389a2484f8b4
Lyuma <xn.lyuma@gmail.com>
2020-11-04 04:07:52 -0800

    Add missing divide by M_PI in toon and lambert_wrap diffuse modes

--- servers/rendering/renderer_rd/shaders/scene_forward.glsl
+++ servers/rendering/renderer_rd/shaders/scene_forward.glsl
@@ -611,7 +611,7 @@ LIGHT_SHADER_CODE
 
 #if defined(DIFFUSE_LAMBERT_WRAP)
 		// energy conserving lambert wrap shader
-		diffuse_brdf_NL = max(0.0, (NdotL + roughness) / ((1.0 + roughness) * (1.0 + roughness)));
+		diffuse_brdf_NL = max(0.0, (NdotL + roughness) / ((1.0 + roughness) * (1.0 + roughness))) * (1.0 / M_PI);
 
 #elif defined(DIFFUSE_OREN_NAYAR)
 
@@ -631,7 +631,7 @@ LIGHT_SHADER_CODE
 
 #elif defined(DIFFUSE_TOON)
 
-		diffuse_brdf_NL = smoothstep(-roughness, max(roughness, 0.01), NdotL);
+		diffuse_brdf_NL = smoothstep(-roughness, max(roughness, 0.01), NdotL) * (1.0 / M_PI);
 
 #elif defined(DIFFUSE_BURLEY)
 
