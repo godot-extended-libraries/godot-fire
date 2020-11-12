59597078fdb2268f01b33ddb58ee0cb18cd024f5
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-11-11 17:20:01 -0800

    Import gltf module.

---
+++ modules/gltf/gltf_light.h
@@ -0,0 +1,97 @@
+/*************************************************************************/
+/*  gltf_light.h                                                         */
+/*************************************************************************/
+/*                       This file is part of:                           */
+/*                           GODOT ENGINE                                */
+/*                      https://godotengine.org                          */
+/*************************************************************************/
+/* Copyright (c) 2007-2020 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2020 Godot Engine contributors (cf. AUTHORS.md).   */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+
+#ifndef GLTF_LIGHT_H
+#define GLTF_LIGHT_H
+
+#include "core/config/engine.h"
+#include "core/io/resource.h"
+
+class GLTFLight : public Resource {
+	GDCLASS(GLTFLight, Resource);
+
+protected:
+	static void _bind_methods();
+
+public:
+	Color color;
+	float intensity;
+	String type;
+	float range;
+	float inner_cone_angle;
+	float outer_cone_angle;
+
+	Color get_color() {
+		return this->color;
+	}
+	void set_color(Color p_color) {
+		this->color = p_color;
+	}
+
+	float get_intensity() {
+		return this->intensity;
+	}
+	void set_intensity(float p_intensity) {
+		this->intensity = p_intensity;
+	}
+
+	String get_type() {
+		return this->type;
+	}
+	void set_type(String p_type) {
+		this->type = p_type;
+	}
+
+	float get_range() {
+		return this->range;
+	}
+	void set_range(float p_range) {
+		this->range = p_range;
+	}
+
+	float get_inner_cone_angle() {
+		return this->inner_cone_angle;
+	}
+	void set_inner_cone_angle(float p_inner_cone_angle) {
+		this->inner_cone_angle = p_inner_cone_angle;
+	}
+
+	float get_outer_cone_angle() {
+		return this->outer_cone_angle;
+	}
+	void set_outer_cone_angle(float p_outer_cone_angle) {
+		this->outer_cone_angle = p_outer_cone_angle;
+	}
+
+	GLTFLight() {}
+	~GLTFLight() {}
+};
+
+#endif
