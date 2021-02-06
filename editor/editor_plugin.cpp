b34acfc015447e004b87b178b577550e3e7016eb
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-02-06 11:17:24 -0800

    Porting SkeletonEditorGizmo for 4.0

--- editor/editor_plugin.cpp
+++ editor/editor_plugin.cpp
@@ -575,7 +575,7 @@ int EditorPlugin::update_overlays() const {
 	}
 }
 
-bool EditorPlugin::forward_spatial_gui_input(Camera3D *p_camera, const Ref<InputEvent> &p_event) {
+bool EditorPlugin::forward_spatial_gui_input(int p_index, Camera3D *p_camera, const Ref<InputEvent> &p_event) {
 	if (get_script_instance() && get_script_instance()->has_method("forward_spatial_gui_input")) {
 		return get_script_instance()->call("forward_spatial_gui_input", p_camera, p_event);
 	}
