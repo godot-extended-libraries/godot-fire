dcb84c1a12b5eab9fc3b6f8131a725f08248cb77
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-05-25 08:18:04 -0700

    Restore old code.

--- scene/3d/skeleton.cpp
+++ scene/3d/skeleton.cpp
@@ -82,11 +82,12 @@ bool Skeleton::_set(const StringName &p_path, const Variant &p_value) {
 			Transform pose = bones[bone].pose;
 			pose.origin = p_value;
 			bones.write[bone].pose = pose;
-		} else if (property == "rotation_quat") {
+		} else if (property == "rotation_quat_log") {
 			ERR_FAIL_INDEX_V(bone, bones.size(), false);
 			Transform pose = bones[bone].pose;
 			Vector3 scale = pose.basis.get_scale();
-			Quat rot = p_value;	
+			Quat rot = p_value;
+			rot = rot.exp();	
 			pose.basis.set_quat_scale(rot, scale);
 			bones.write[bone].pose = pose;
 		} else if (property == "rotation_quat_log") {
@@ -165,9 +166,10 @@ bool Skeleton::_get(const StringName &p_path, Variant &r_ret) const {
 		if (property == "translation") {
 			r_ret = bones[bone].pose.origin;
 			return true;
-		} else if (property == "rotation_quat") {
+		} else if (property == "rotation_quat_log") {
 			Basis rot_basis = bones[bone].pose.basis;
 			Quat rot = rot_basis.get_rotation_quat();
+			rot = rot.log();
 			r_ret = rot;
 			return true;
 		} else if (property == "rotation_quat_log") {
